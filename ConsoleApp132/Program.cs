4d48f6f442a9acae3f30a7a5b4afcbb48a3a00b8
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-09-12 10:45:27 +0330

    Add files via upload

---
+++ ConsoleApp132/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp132
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Square of integer 7 is:{Square(7)}");
+            Console.WriteLine($"Square of double 7.5 is:{Square(7.5)}");
+            Console.ReadLine();
+        }
+        static int Square(int intValue)
+        {
+            Console.WriteLine($"Called with square int argument:{intValue}");
+            return intValue * intValue;
+        }
+        static double Square(double doubleValue)
+        {
+            Console.WriteLine($"Called with square double argument:{doubleValue}");
+            return doubleValue * doubleValue;
+        }
+         
+            
+    }
+}
